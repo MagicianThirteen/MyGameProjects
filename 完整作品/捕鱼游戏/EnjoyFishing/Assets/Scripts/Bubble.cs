ae21cc3264ab7cd7ea9ace1bbcd4b9ea43b4c062
MagicianThirteen <magicianthirteen@gmail.com>
2020-08-04 11:16:21 +0800

    开始场景，产生泡泡

---
+++ 完整作品/捕鱼游戏/EnjoyFishing/Assets/Scripts/Bubble.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    public float moveSpeed;
+    public bool isGameBubble;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //第一个是不要泡泡飞射出去效果，第二个是要泡泡飞射出去效果
+        if (!isGameBubble)
+        {
+            
+            moveSpeed = Random.Range(2f, 4f);
+            Destroy(this.gameObject, Random.Range(0.5f,1f));
+        }
+        else
+        {
+            //这里7-10秒能走出屏幕
+            moveSpeed = Random.Range(40f, 100f);
+            Destroy(this.gameObject, Random.Range(7f, 10f));
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-Vector3.right * moveSpeed * Time.deltaTime,Space.Self);
+    }
+}
---
+++ 完整作品/捕鱼游戏/EnjoyFishing/Assets/Scripts/CreateBubbles.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+///  每隔一段时间，产生泡泡
+/// </summary>
+public class CreateBubbles : MonoBehaviour
+{
+    public GameObject bubble;
+    public Transform bg;
+    private float timeval;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeval > 1)
+        {
+            for(int i = 0; i < 2; i++)
+            {
+                //一秒产生一个泡泡
+                Invoke("InitBubbles", 1);
+            }
+            timeval = 0;
+        }
+        else
+        {
+            timeval += Time.deltaTime;
+        }
+    }
+
+    private void InitBubbles()
+    {
+        Instantiate(bubble, transform.position, Quaternion.Euler(0, 0, Random.Range(-80, 0)), bg);
+    }
+}
